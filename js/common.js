9456b6ae4f3990134c0820ee5ebe8739077b7868
NikRazumkov <Nik.Razumkov@gmail.com>
2016-12-07 13:25:11 +0300

    07.12.2016

--- js/common.js
+++ js/common.js
@@ -1,135 +1,5 @@
 $(function() {
 
-		$(".porfolio-item").each(function() {
-		$(this).magnificPopup({
-			delegate: 'a',
-			mainClass: 'mfp-zoom-in',
-			type: 'image',
-			tLoading: '',
-			gallery:{
-				enabled: true,
-			},
-			removalDelay: 300,
-			callbacks: {
-				beforeChange: function() {
-					this.items[0].src = this.items[0].src + '?=' + Math.random(); 
-				},
-				open: function() {
-					$.magnificPopup.instance.next = function() {
-						var self = this;
-						self.wrap.removeClass('mfp-image-loaded');
-						setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
-					}
-					$.magnificPopup.instance.prev = function() {
-						var self = this;
-						self.wrap.removeClass('mfp-image-loaded');
-						setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
-					}
-				},
-				imageLoadComplete: function() { 
-					var self = this;
-					setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
-				}
-			}
-		});
-	});
-
-	$(".carousel-brands").owlCarousel({
-		loop:true,
-		margin: 30,
-		nav: true,
-		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-		responsive:{
-				0:{
-						items:1,
-				},
-				520:{
-						items:1,
-				},
-				560:{
-						items:2,
-				},
-				768:{
-						items:2,
-				},
-				992:{
-						items:3,
-				},
-				1200:{
-						items:4,
-				}
-		}
-	});
-		
-	$(window).resize(function() {
-		heightses();
-	});
-
-
-	$(window).scroll(function() {
-		if($(this).scrollTop() > $(this).height()) {
-			$(".top").addClass("active");
-		} else {
-			$(".top").removeClass("active");
-		}
-	});
-
-	$(".mouse-icon").click(function() {
-		$("html, body").animate({
-			scrollTop : $(".s-rent").offset().top
-		}, 800);
-	});
-
-	$(".s-number").waypoint(function() {
-		
-		$({blurRadius: 5}).animate({blurRadius: 0}, {
-			duration: 1200,
-			easing: 'swing',
-			step: function() {
-				$(".s-number-item h3 span").css({
-					"-webkit-filter": "blur("+this.blurRadius+"px)",
-					"filter": "blur("+this.blurRadius+"px)"
-				});
-			}
-		});
-		var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
-		$(".s-number-item h3 span").each(function() {
-			var tcount = $(this).data("count");
-			$(this).animateNumber({ number: tcount,
-				easing: 'easeInQuad',
-				"font-size": "1.7em",
-				numberStep: comma_separator_number_step},
-				1200);
-		});
-		this.destroy();
-
-	}, {
-		offset: '100%'
-	});
-
-
-	$(".toggle-mnu").click(function() {
-		$(this).toggleClass("on");
-		$(this).next().find(".main-mnu").slideToggle();
-		return false;
-	});
-
-		$(".portfolio-item").each(function(e) {
-
-		var th = $(this);
-
-		th.attr("href", "#portfolio-img-" + e)
-			.find(".portfolio-popup")
-				.attr("id", "portfolio-img-" + e);
-
-	});
-
-	$(".portfolio-item, a[href='#callback']").magnificPopup({
-		mainClass: 'my-mfp-zoom-in',
-		removalDelay: 300,
-		type: 'inline',
-	});
-
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
@@ -155,46 +25,23 @@ $(function() {
 		return false;
 	});
 
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
-
-	};
-
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
 });
 
-	//Replace all SVG images with inline SVG
-	$('img.img-svg').each(function(){
-		var $img = $(this);
-		var imgClass = $img.attr('class');
-		var imgURL = $img.attr('src');
+//Animate CSS + WayPoints javaScript Plugin
+//Example: $(".element").animated("zoomInUp");
+//Author URL: http://webdesign-master.ru
+(function($) {
+	$.fn.animated = function(inEffect) {
+		$(this).each(function() {
+			var ths = $(this);
+			ths.css("opacity", "0").addClass("animated").waypoint(function(dir) {
+				if (dir === "down") {
+					ths.addClass(inEffect).css("opacity", "1");
+				};
+			}, {
+				offset: "90%"
+			});
 
-		$.get(imgURL, function(data) {
-				// Get the SVG tag, ignore the rest
-				var $svg = $(data).find('svg');
-
-				// Add replaced image's classes to the new SVG
-				if(typeof imgClass !== 'undefined') {
-					$svg = $svg.attr('class', imgClass+' replaced-svg');
-				}
-
-				// Remove any invalid XML tags as per http://validator.w3.org
-				$svg = $svg.removeAttr('xmlns:a');
-
-				// Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-				if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-					$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-				}
-
-				// Replace image with new SVG
-				$img.replaceWith($svg);
-
-			}, 'xml');
-
-	});
+		});
+	};
+})(jQuery);
