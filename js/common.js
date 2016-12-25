06a1ae78f02901bdc23daadf43a061ac75139dd3
nikrazumkov <nik.razunkov@gmail.com>
2016-12-25 23:48:54 +0300

    new site

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 $(function() {
 
-	$('.till-item-2').magnificPopup({
+	$('.till-item-all-img').magnificPopup({
 		delegate: 'a',
 		type: 'image',
 		tLoading: 'Loading image #%curr%...',
@@ -11,6 +11,7 @@ $(function() {
 			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
 		},
 		image: {
+			
 			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
 			titleSrc: function(item) {
 				return item.el.attr('title') + '<small>от KorolMebel</small>';
