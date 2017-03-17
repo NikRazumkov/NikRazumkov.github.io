5b903d497c46413da3884ce0f5bc195d3cc204c4
nikrazumkov <nik.razunkov@gmail.com>
2017-03-17 13:36:23 +0300

    test

--- js/common.js
+++ js/common.js
@@ -1,83 +1,4 @@
-$(document).ready(function () {
-
-	function heightDetect() {
-		$(".main_head").css("height", $(window).height());
-	}
-	heightDetect();
-	$(window).resize(function() {
-		heightDetect();
-	})
-
-	$(".loaderInner").fadeOut('slow'); 
-	$(".loader").delay(400).fadeOut();
-
-	$('.main-head').parallax({imageSrc: 'img/top-img.png' });
-});
-
 $(function() {
-
-
-	$('.till-item-all-img').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>от KorolMebel</small>';
-			}
-		}
-	});
-
-	$(".till-item-all").equalHeights();
-
-
-	$(".mobile-mnu").click(function(){
-		$(this).toggleClass("on");
-		$(".sf-menu").slideToggle();
-	})
-
-		//top-menu
-	$(".top-line .sf-menu").superfish({
-		cssArrows: false,
-		hoverClass:'no-class',
-		delay: 200
-	});
-
-	// mobile-menu
-
-	$(".sf-menu").after("<div id='my-menu'>");
-	$(".sf-menu").clone().appendTo("#my-menu");
-	$("#my-menu").find("*").attr("style", "");
-	$("#my-menu").find("ul").removeClass("sf-menu", "");
-	$("#my-menu").mmenu({
-		extension : [ 'widescreen', 'theme-white', 'efect-menu-slide', 'pagim-black'],
-		navbar: {
-			title : "Меню"
-		}
-	});
-
-
-	var api = $("#my-menu").data("mmenu");
-	api.bind("closed", function (){
-		$(".toggle-mnu").removeClass("on");	
-	});
-
-	$(".mobile-mnu").click(function() {
-		var mmAPI = $("#my-menu").data("mmenu");
-		mmAPI.open();
-		var thiss = $(this).find(".toggle-mnu");
-		thiss.toggleClass("on");
-		$(".main-mnu").slideToggle();
-		return false;	
-	});
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
@@ -88,40 +9,23 @@ $(function() {
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
-	$(".callback").submit(function() { //Change
+	$("form").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
-			$(".succes").addClass(".visible");
+			alert("Thank you!");
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");
-				$(".succes").addClass(".visible");
-				$.magnificPopup.close();
-			}, 3000);
+			}, 1000);
 		});
 		return false;
 	});
 
-	$('.popup-with-move-anim').magnificPopup({
-		type: 'inline',
 
-		fixedContentPos: false,
-		fixedBgPos: true,
-
-		overflowY: 'auto',
-
-		closeBtnInside: true,
-		preloader: false,
-		
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
-	});
-
-
+	$(".feedback-text-value").equalHeights();
 
 });
